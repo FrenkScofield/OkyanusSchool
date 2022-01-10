97e51b9e8ea64e81f98781bdfdc8b89f97244f29
FRENK SCOFIELD <kam_alibeyli@mail.ru>
2022-01-10 18:40:08 +0300

    01/10/2022 -18:40- Student information created in the admin panel is ready to add

--- OkyanusSchool/Areas/WebCms/Controllers/OgrenciBilgiController.cs
+++ OkyanusSchool/Areas/WebCms/Controllers/OgrenciBilgiController.cs
@@ -43,10 +43,8 @@ namespace OkyanusSchool.Areas.WebCms.Controllers
         [HttpPost]
         public IActionResult PullOgrenciBilgi(int? ISLEM)
         {
-         
             SqlParameter[] param = new SqlParameter[] 
             {
-                
                 new SqlParameter()
                 {
                     ParameterName = "ISLEM",
@@ -54,7 +52,6 @@ namespace OkyanusSchool.Areas.WebCms.Controllers
                     Direction = ParameterDirection.Input,
                     IsNullable = true,
                     Value = ISLEM
-
                 },
 
                 new SqlParameter()
@@ -64,7 +61,6 @@ namespace OkyanusSchool.Areas.WebCms.Controllers
                     Direction = ParameterDirection.Input,
                     IsNullable = true,
                     Value = DBNull.Value
-          
                 }
             };
 
@@ -76,14 +72,130 @@ namespace OkyanusSchool.Areas.WebCms.Controllers
 
             TempData["Orgs"] = JsonConvert.SerializeObject(_ogrenciListVM.OgrenciBilgis); ;
 
-            return RedirectToAction("Index"); ;
-
+            return RedirectToAction("Index"); 
         }
 
         [HttpGet]
-        public async Task<IActionResult> CreateOgrenciBilgi()
+        public  IActionResult CreateOgrenciBilgi()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateOgrenciBilgi(int? ISLEM,  string AD, string SOYAD, string TC_KIMLIK,
+
+                                                            string DOGUM_TARIHI , string DOGUM_YERI, string CEP_TELEFONU,
+
+                                                            string EV_TELEFONU, string EMAIL, string UYRUK, string GUNCEL_TARIH) 
+        {
+            List<SqlParameter> paramInsert = new List<SqlParameter>
+           {
+                new SqlParameter()
+                {
+                    ParameterName = "ISLEM",
+                    SqlDbType = SqlDbType.Int,
+                    Direction = ParameterDirection.Input,
+                    IsNullable = true,
+                    Value = ISLEM
+                },
+
+                new SqlParameter()
+                {
+                    ParameterName = "ID",
+                    SqlDbType = SqlDbType.Int,
+                    Direction = ParameterDirection.Input,
+                    IsNullable = true,
+                    Value = DBNull.Value
+                },
+                 new SqlParameter()
+                {
+                    ParameterName = "AD",
+                    SqlDbType = SqlDbType.NVarChar,
+                    Direction = ParameterDirection.Input,
+                    IsNullable = true,
+                    Value = AD
+                },
+                  new SqlParameter()
+                {
+                    ParameterName = "SOYAD",
+                    SqlDbType = SqlDbType.NVarChar,
+                    Direction = ParameterDirection.Input,
+                    IsNullable = true,
+                    Value = SOYAD
+                },
+                  new SqlParameter()
+                {
+                    ParameterName = "TC_KIMLIK",
+                    SqlDbType = SqlDbType.NVarChar,
+                    Direction = ParameterDirection.Input,
+                    IsNullable = true,
+                    Value = TC_KIMLIK
+                },
+                  new SqlParameter()
+                {
+                    ParameterName = "DOGUM_TARIHI",
+                    SqlDbType = SqlDbType.NVarChar,
+                    Direction = ParameterDirection.Input,
+                    IsNullable = true,
+                    Value = DOGUM_TARIHI
+                },
+                  new SqlParameter()
+                {
+                    ParameterName = "DOGUM_YERI",
+                    SqlDbType = SqlDbType.NVarChar,
+                    Direction = ParameterDirection.Input,
+                    IsNullable = true,
+                    Value = DOGUM_YERI
+                },
+                  new SqlParameter()
+                {
+                    ParameterName = "CEP_TELEFONU",
+                    SqlDbType = SqlDbType.NVarChar,
+                    Direction = ParameterDirection.Input,
+                    IsNullable = true,
+                    Value = CEP_TELEFONU
+                },
+                  new SqlParameter()
+                {
+                    ParameterName = "EV_TELEFONU",
+                    SqlDbType = SqlDbType.NVarChar,
+                    Direction = ParameterDirection.Input,
+                    IsNullable = true,
+                    Value = EV_TELEFONU
+                },
+                  new SqlParameter()
+                {
+                    ParameterName = "EMAIL",
+                    SqlDbType = SqlDbType.NVarChar,
+                    Direction = ParameterDirection.Input,
+                    IsNullable = true,
+                    Value = EMAIL
+                },
+                  new SqlParameter()
+                {
+                    ParameterName = "UYRUK",
+                    SqlDbType = SqlDbType.NVarChar,
+                    Direction = ParameterDirection.Input,
+                    IsNullable = true,
+                    Value = UYRUK
+                },
+                  new SqlParameter()
+                {
+                    ParameterName = "GUNCEL_TARIH",
+                    SqlDbType = SqlDbType.NVarChar,
+                    Direction = ParameterDirection.Input,
+                    IsNullable = true,
+                    Value = GUNCEL_TARIH
+                }
+           };
+
+            string sql = $"EXEC sp_OgrenciBilgi @ISLEM, @ID, @AD," +
+                $" @SOYAD, @TC_KIMLIK ,@DOGUM_TARIHI, @DOGUM_YERI," +
+                $" @CEP_TELEFONU, @EV_TELEFONU, @EMAIL, @UYRUK, @GUNCEL_TARIH";
+
+            _context.Database.ExecuteSqlRaw(sql, paramInsert);
+
+            return RedirectToAction("Index");
         }
     }
 }
